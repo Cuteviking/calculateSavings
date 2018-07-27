43bbddc0e5470fbe93375e7dbd9ae53474ca704a
cuteviking <pierrenorrbrink@gmail.com>
2018-07-27 14:09:57 +0200

    calculate goal with monthly saving, years and profit

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,48 @@
 <template>
   <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        calculate-savings
-      </h1>
-      <h2 class="subtitle">
-        Calculate how much user need to save each month to reach goal
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
+    <p>Calculate amount to save each month</p>
+    <form>
+
+      Monthly Saving: <input v-model="monthSave" type="number" name=""><br>
+      Years: <input v-model="time" type="number" name=""><br>
+      Profit: <input v-model="profit" type="number" name=""><br>
+    </form>
+    <p>Your goal will be {{fromMonthSaveToGoal}} kr</p>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
 
 export default {
-  components: {
-    AppLogo
+  data() {
+    return{
+      time: 1,
+      profit: 0,
+      monthSave: 10000
+    }
+  },
+  computed: {
+    fromMonthSaveToGoal: function() {
+      const amountOfMonths = this.time*12;
+      const profitDecimal = (this.profit / 100) + 1;
+      var sum = 0;
+
+      for(var month = 0; month < amountOfMonths; month++) {
+        sum += Number(this.monthSave);
+
+        var monthInPrecentage = (month + 1) / 12;
+        if(monthInPrecentage % 1 == 0) {
+          sum = sum * profitDecimal;
+        }
+      }
+
+      return sum;
+    }
   }
 }
 </script>
 
 <style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
 
